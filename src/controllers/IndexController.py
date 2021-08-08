83dc70e059573ef7c72283a2dfe1e5d82fcd3cd2
José Mariano <jose.mariano@climatempo.com.br>
2021-08-08 18:37:21 -0300

    feat: Menu principal criado

--- src/controllers/IndexController.py
+++ src/controllers/IndexController.py
@@ -2,13 +2,11 @@ from src.views.CLI import CLI as Interface
 
 class IndexController:
 	def __init__(self):
-		self.interface = Interface()
-
-	def start(self):
-		self.interface.getPersonData()
+		self.interface = Interface(self)
+		self.interface.start()
 
 
 
 
 if __name__ == '__main__':
-	IndexController().start()
+	IndexController()
