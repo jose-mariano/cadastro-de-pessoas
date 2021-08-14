a5101c57f324bdef1640b3c0f025951bc2774811
José Mariano <jose.mariano@climatempo.com.br>
2021-08-13 21:32:16 -0300

    feat: Adicionada funcao para validar data de aniversario

--- src/models/ValidatePersonalData.py
+++ src/models/ValidatePersonalData.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class ValidatePersonalData:
 	def isValidGender(self, gender):
 		genders = ['masculino', 'feminino']
@@ -11,9 +13,25 @@ class ValidatePersonalData:
 	def isValidName(self, name):
 		nameLength = len(name)
 		
-		if ((nameLength == 0) or (nameLength < 3)):
+		if (nameLength < 3):
 			return False
 
 		return True
 
 
+	def isValidBirthDate(self, birthDate):
+		birthDateSplit = birthDate.split('-')
+
+		if (len(birthDateSplit) != 3):
+			return False
+
+		try:
+			year = int(birthDateSplit[0])
+			month = int(birthDateSplit[1])
+			day = int(birthDateSplit[2])
+
+			date(year, month, day)
+		except:
+			return False
+
+		return True
