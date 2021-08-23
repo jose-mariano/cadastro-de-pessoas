77da0bff17a1849988143122b52e28b4b9295fd9
José Mariano <jose.mariano@climatempo.com.br>
2021-08-22 21:20:33 -0300

    style: Melhoria de algumas frases

--- src/views/GUI/WelcomePage.py
+++ src/views/GUI/WelcomePage.py
@@ -4,5 +4,9 @@ from src.views.GUI.Page import Page
 class WelcomePage(Page):
 	def __init__(self, *args, **kwargs):
 		Page.__init__(self, *args, **kwargs)
-		label = tk.Label(self, text="Bem vindo ao sistema de cadastro python!", bg="white")
+		label = tk.Label(
+			self,
+			text="Bem vindo ao sistema de cadastro de pessoas com python!",
+			font=("Arial", 15, "bold")
+		)
 		label.pack(side="top", fill="both", expand=True)
